e556dfa4fee6c7717276e3850fb3891fde6d453e
jonathanjoni <60664830+jonathanjoni@users.noreply.github.com>
2024-04-25 14:41:23 -0300

    Arquivo desnecessario removido

--- lib/apf1_funcoes/ap3.dart
+++ lib/apf1_funcoes/ap3.dart
@@ -1,60 +1,109 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 
 void main() {
-  runApp(AppSorteadorDeCor());
+  runApp(MyApp());
 }
 
-class AppSorteadorDeCor extends StatefulWidget {
-  const AppSorteadorDeCor({Key? key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
-  State<AppSorteadorDeCor> createState() => _AppSorteadorDeCorState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
 }
 
-class _AppSorteadorDeCorState extends State<AppSorteadorDeCor> {
-  String texto = "Sortear Cor";
-  Color cor = Colors.yellow;
+class MyWidget extends StatefulWidget {
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
 
-  List<Color> cores = [
-    Colors.yellow,
-    Colors.amber,
-    Colors.purple,
-    Colors.redAccent,
-    Colors.white,
-    Colors.green,
-    Colors.pinkAccent
-  ];
+class _MyWidgetState extends State<MyWidget> {
+  final random = Random();
 
-  Color sorteaCor() {
-    int indiceCorAleatoria = Random().nextInt(cores.length);
-    return cores[indiceCorAleatoria];
+  var botaoCorreto = 0;
+  var clicks = 0;
+  var ganhou = false;
+  var perdeu = false;
+
+  // Esse método e chamado somente uma vez, ao iniciar o state
+  @override
+  void initState() {
+    super.initState();
+
+    // Escolher um número de 0 a 2 para identificar escolher o botão correto
+    botaoCorreto = random.nextInt(3);
+  }
+
+  // Tratar a tentativa do usuário
+  void tentativa(int opcao) {
+    setState(() {
+      // Verificar se a opção escolhida esta correta
+      if (opcao == botaoCorreto) {
+        ganhou = true;
+      } else {
+        // Se estiver errada, incrementa o contador de clicks
+        clicks++;
+      }
+
+      // Se a quantidade de clicks for maior ou igual a 2, o usuário perdeu
+      if (clicks >= 2 && !ganhou) {
+        perdeu = true;
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              texto,
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: cor, fontSize: 50),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  cor = sorteaCor();
-                });
-              },
-              child: Text("Sortear Cor"),
-            ),
-          ],
+    // Se o usuário ganhou, retorna a mensagem de sucesso com o fundo em verde
+    if (ganhou) {
+      return Container(
+        color: Colors.green,
+        child: const Text('Você ganhou'),
+      );
+    }
+
+    // Se o usuário perdeu, retorna a mensagem de fracasso com o fundo em vermelho
+    if (perdeu) {
+      return Container(
+        color: Colors.red,
+        child: const Text('Você perdeu'),
+      );
+    }
+
+    // Nesse momento o jogo ainda nao foi finalizado
+    return Column(
+      children: [
+        ElevatedButton(
+          child: const Text('A'),
+          onPressed: () {
+            tentativa(0);
+          },
         ),
-      ),
+        ElevatedButton(
+          child: const Text('B'),
+          onPressed: () {
+            tentativa(1);
+          },
+        ),
+        ElevatedButton(
+          child: const Text('C'),
+          onPressed: () {
+            tentativa(2);
+          },
+        ),
+      ],
     );
   }
 }
