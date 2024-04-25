30660a77e5bf528a03c961cc10c7096e148bd5e3
jonathanjoni <60664830+jonathanjoni@users.noreply.github.com>
2024-04-25 00:06:57 -0300

    atv2 finalizada

---
+++ lib/apf1_funcoes/ap2.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: OpcaoAleatoriaApp(),
+  ));
+}
+
+class OpcaoAleatoriaApp extends StatefulWidget {
+  const OpcaoAleatoriaApp({Key? key}) : super(key: key);
+
+  @override
+  State<OpcaoAleatoriaApp> createState() => _OpcaoAleatoriaAppState();
+}
+
+class _OpcaoAleatoriaAppState extends State<OpcaoAleatoriaApp> {
+  late int numeroCorreto;
+  int numeroCliques = 0;
+  Color corDoFundo = Colors.black;
+
+  @override
+  void initState() {
+    super.initState();
+
+    numeroCorreto = sorteaBotao();
+  }
+
+  int sorteaBotao() {
+    final numeroAleatorio = Random().nextInt(2);
+    return numeroAleatorio;
+  }
+
+  void verificaSeEhBotaoCorreto(int botao) {
+    if (botao == numeroCorreto) {
+      setState(() {
+        corDoFundo = Colors.green;
+      });
+      print('você acertou');
+    } else if (numeroCliques < 1) {
+      numeroCliques++;
+      print('você tem mais uma tentativa');
+    } else {
+      setState(() {
+        corDoFundo = Colors.red;
+      });
+      print('Você perdeu!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: corDoFundo,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  verificaSeEhBotaoCorreto(0);
+                },
+                child: Text("Botao1"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  verificaSeEhBotaoCorreto(1);
+                },
+                child: Text('Botao2'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  verificaSeEhBotaoCorreto(2);
+                },
+                child: Text('Botao3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
